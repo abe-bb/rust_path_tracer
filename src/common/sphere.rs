38aa39365794955863a9f6e34be6ea13aac4c978
Abram Clements <abram.j.clements@gmail.com>
2021-03-03 11:34:48 -0700

    add sphere intersection testing

--- src/common/sphere.rs
+++ src/common/sphere.rs
@@ -4,6 +4,7 @@ use crate::common::Vec3;
 use crate::common::VertexComp;
 use crate::common::Visible;
 
+#[derive(Debug)]
 pub struct Sphere<T: VertexComp> {
     center: Vec3<T>,
     radius: T,
@@ -19,15 +20,15 @@ impl<T: VertexComp> Visible<T> for Sphere<T> {
     fn intersect(&self, ray: &Ray<T>) -> Option<Intersection<T>> {
         let oc = self.center.sub(&ray.origin);
         let tca = ray.direction.dot(&oc);
-        let d2 = oc.dist_sqrd();
-        let inside_sphere = d2 < self.radius;
+        let oc_d2 = oc.mag_sqrd();
+        let inside_sphere = oc_d2 < self.radius;
 
         // Ray does not intersect sphere
         if tca < T::zero() && !inside_sphere {
             return None;
         }
 
-        let thc2 = (self.radius * self.radius) - (d2 * d2) + (tca * tca);
+        let thc2 = (self.radius * self.radius) - oc_d2 + (tca * tca);
         // Sphere is behind the ray. Ray does not intersect sphere
         if thc2 < T::zero() {
             return None;
@@ -46,3 +47,53 @@ impl<T: VertexComp> Visible<T> for Sphere<T> {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    // tests that a ray along the x-axis intersects a sphere at the origin at the correct place
+    fn intersect_origin_x_axis() {
+        let ray = Ray::new(
+            Vec3::new(10.0, 0.0, 0.0),
+            Vec3::new(-1.0, 0.0, 0.0));
+
+        let sphere = Sphere::new(
+            Vec3::new(0.0, 0.0, 0.0),
+            1.0
+        );
+
+        let expected_point = Vec3::new(1.0, 0.0, 0.0);
+
+
+
+        // Panic of intersect returns none. That's a bug
+        let intersection_point = sphere.intersect(&ray).unwrap();
+
+        assert_eq!(expected_point, intersection_point.point)
+    }
+
+    #[test]
+    // tests a ray intersection that does not pass through the origin of the sphere
+    fn intersect() {
+        let ray = Ray::new(
+            Vec3::new(10.0, 0.5, 0.0),
+            Vec3::new(-1.0, 0.0, 0.0));
+
+        let sphere = Sphere::new(
+            Vec3::new(0.0, 0.0, 0.0),
+            1.0
+        );
+
+        let expected_point = Vec3::new(0.8660254037844393, 0.5, 0.0);
+
+
+
+        // Panic of intersect returns none. That's a bug
+        let intersection_point = sphere.intersect(&ray).unwrap();
+
+        assert_eq!(expected_point, intersection_point.point)
+    }
+}
+
