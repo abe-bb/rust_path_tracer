38be200ff94ba85b3457992429a3b8df2bacf1d8
Abram Clements <abram.j.clements@gmail.com>
2021-03-03 11:26:02 -0700

    add Vec3 vector normalization tests

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,13 +1,17 @@
 mod sphere;
 
 use num;
+use std::fmt::Debug;
 
-pub trait VertexComp: num::Float {}
+// Traits defining needed operations for vectors
+pub trait VertexComp: num::Float + Debug {}
+impl<T> VertexComp for T where T: num::Float + Debug {}
 
 pub trait Visible<T: VertexComp> {
     fn intersect(&self, ray: &Ray<T>) -> Option<Intersection<T>>;
 }
 
+#[derive(PartialEq, Debug)]
 pub struct Vec3<T: VertexComp> {
     x: T,
     y: T,
@@ -15,11 +19,12 @@ pub struct Vec3<T: VertexComp> {
 }
 
 impl<T: VertexComp> Vec3<T> {
-    fn new(x: T, y: T, z: T) -> Vec3<T> {
+    pub fn new(x: T, y: T, z: T) -> Vec3<T> {
         Vec3 { x, y, z }
     }
 
-    fn dist_sqrd(&self) -> T {
+    // magnitude squared of the vector
+    fn mag_sqrd(&self) -> T {
         (self.x * self.x) + (self.y * self.y) + (self.z * self.z)
     }
 
@@ -47,7 +52,7 @@ impl<T: VertexComp> Vec3<T> {
         }
     }
 
-    fn add(&self, other: &Vec3<T>) -> Vec3<T> {
+    pub fn add(&self, other: &Vec3<T>) -> Vec3<T> {
         Vec3 {
             x: self.x + other.x,
             y: self.y + other.y,
@@ -55,15 +60,17 @@ impl<T: VertexComp> Vec3<T> {
         }
     }
 
-    fn dot(&self, other: &Vec3<T>) -> T {
+    pub fn dot(&self, other: &Vec3<T>) -> T {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
-    fn normalize(&mut self) {
-        let dist = self.dist_sqrd();
+    pub fn normalize(mut self) -> Self {
+        let dist = self.mag_sqrd().sqrt();
         self.x = self.x / dist;
         self.y = self.y / dist;
         self.z = self.z / dist;
+
+        self
     }
 }
 
@@ -74,11 +81,38 @@ pub struct Ray<T: VertexComp> {
 
 impl<T: VertexComp> Ray<T> {
     fn new(origin: Vec3<T>, direction: Vec3<T>) -> Ray<T> {
-        Ray { origin, direction }
+        Ray {
+            origin,
+            direction: direction.normalize(),
+        }
     }
 }
 
+#[derive(PartialEq, Debug)]
 pub struct Intersection<T: VertexComp> {
     point: Vec3<T>,
     normal: Vec3<T>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normalize_unit_vectors() {
+        let unit_x = Vec3::new(-1.0, 0.0, 0.0);
+        let expected = Vec3::new(-1.0, 0.0, 0.0);
+
+        assert_eq!(unit_x.normalize(), expected);
+    }
+
+    #[test]
+    fn normalize() {
+        let vector = Vec3::new(1.0, 1.0, 0.0);
+
+        let expected = Vec3::new(1.0 / 2.0_f64.sqrt(), 1.0 / 2.0_f64.sqrt(), 0.0);
+
+        assert_eq!(expected, vector.normalize());
+    }
+
 }
