32a94d541bdff11e9cb9f38947f928232976999b
Abram Clements <abram.j.clements@gmail.com>
2021-03-06 14:19:55 -0700

    Add basic scene creation tools

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,9 +1,24 @@
 use crate::common::VertexFormat;
 use crate::scene::camera::Camera;
+use crate::scene::visible::Visible;
 
 mod camera;
 mod visible;
 
 struct Scene<T: VertexFormat> {
     camera: Camera<T>,
+    visibles: Vec<Box<dyn Visible<T>>>,
+}
+
+impl<T: VertexFormat> Scene<T> {
+    pub fn new(camera: Camera<T>) -> Scene<T> {
+        Scene {
+            camera,
+            visibles: Vec::new(),
+        }
+    }
+
+    pub fn add_visible(&mut self, visible: Box<dyn Visible<T>>) {
+        self.visibles.push(visible);
+    }
 }
