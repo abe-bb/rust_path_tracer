18aa503ad990d3cdbce2632d4ddd790210970c9e
Abram Clements <abram.j.clements@gmail.com>
2021-03-10 12:57:00 -0700

    add light sources to scene, add ability to set ambient light

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,5 +1,6 @@
-use crate::common::{Vec3, VertexFormat};
+use crate::common::{Color, Vec3, VertexFormat};
 use crate::scene::camera::Camera;
+use crate::scene::light::LightSource;
 use crate::scene::visible::Visible;
 
 mod camera;
@@ -9,19 +10,30 @@ mod visible;
 struct Scene<T: VertexFormat> {
     camera: Camera<T>,
     visibles: Vec<Box<dyn Visible<T>>>,
-    ambient_color: Vec3<T>,
+    ambient_color: Color<T>,
+    lights: Vec<Box<dyn LightSource<T>>>,
 }
 
 impl<T: VertexFormat> Scene<T> {
-    pub fn new(camera: Camera<T>) -> Scene<T> {
+    pub fn new(camera: Camera<T>, ambient_color: Color<T>) -> Scene<T> {
         Scene {
             camera,
             visibles: Vec::new(),
-            ambient_color: Vec3::new(T::zero(), T::zero(), T::zero()),
+            ambient_color,
+            lights: Vec::new(),
         }
     }
 
+    // Mutable self borrows
     pub fn add_visible(&mut self, visible: Box<dyn Visible<T>>) {
         self.visibles.push(visible);
+    }
+
+    pub fn add_light(&mut self, light: Box<dyn LightSource<T>>) {
+        self.lights.push(light);
+    }
+
+    pub fn set_ambient_color(&mut self, color: Color<T>) {
+        self.ambient_color = color;
     }
 }
