8a7cb94b8edfbbab5857d0304b0580f1342fd847
Abram Clements <abram.j.clements@gmail.com>
2021-03-10 20:37:59 -0700

    add ray creation testing, fix camera fov bug

--- src/scene/camera.rs
+++ src/scene/camera.rs
@@ -28,7 +28,7 @@ impl<T: VertexFormat> Camera<T> {
         let aspect_ratio = width / height;
 
         let vertical_fov = T::from(2.0).unwrap()
-            * ((horizontal_fov / T::from(2.0).unwrap()).tan() * aspect_ratio).atan();
+            * ((horizontal_fov / T::from(2.0).unwrap()).tan() * (height / width)).atan();
 
         let horizontal_distance = (horizontal_fov / T::from(2.0).unwrap()).tan();
         let vertical_distance = (vertical_fov / T::from(2.0).unwrap()).tan();
@@ -56,8 +56,10 @@ impl<T: VertexFormat> Camera<T> {
     }
 
     pub fn ray(&self, i: T, j: T) -> Ray<T> {
-        let u = (i - T::zero()) * ((self.view_max.x - self.view_min.x) / self.x_res);
-        let v = (j - T::zero()) * ((self.view_max.y - self.view_min.y) / self.y_res);
+        let u =
+            (i - T::zero()) * ((self.view_max.x - self.view_min.x) / self.x_res) + self.view_min.x;
+        let v =
+            (j - T::zero()) * ((self.view_max.y - self.view_min.y) / self.y_res) + self.view_min.y;
         let w = T::zero();
 
         // location of pixel (or sub pixel) in world space
@@ -72,5 +74,29 @@ impl<T: VertexFormat> Camera<T> {
         let direction = pixel_loc.sub(&origin).normalize();
 
         Ray::new(origin, direction)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_opposite_corner_ray_creation() {
+        let look_at = Vec3::new(0.0, 0.0, 0.0);
+        let look_from = Vec3::new(0.0, 0.0, 10.0);
+        let up = Vec3::new(0.0, 1.0, 0.0);
+        let x = 1920.0;
+        let y = 1080.0;
+        let h_fov = 1.22173;
+
+        let camera = Camera::new(look_at, look_from, up, x, y, h_fov);
+
+        let ray1 = camera.ray(0.0, 0.0);
+        let ray2 = camera.ray(1920.0, 1080.0);
+
+        assert_eq!(ray1.direction.x, -ray2.direction.x);
+        assert_eq!(ray1.direction.y, -ray2.direction.y);
+        assert_eq!(ray1.direction.z, ray2.direction.z);
     }
 }
